e67b9ebc580203e6eee3de49f40c6f33514691f0
vadimko4 <vad.informat@gmail.com>
2025-07-16 01:01:31 +0400

    update static file processing, add home path

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,7 @@ from catalog.views import home, contacts
 app_name = CatalogConfig.name
 
 urlpatterns = [
+    path('', home, name='home'),
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
 ]
