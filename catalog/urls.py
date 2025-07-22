dac3bd365d0c66262e1670154479b68e8aa66481
vadimko4 <vad.informat@gmail.com>
2025-07-23 02:37:43 +0400

    update models, migrations, home

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home, products_detail
+from catalog.views import contacts, products_detail, home
 
 app_name = CatalogConfig.name
 
--- catalog/views.py
+++ catalog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
 
 
 def contacts(request):
@@ -15,3 +15,8 @@ def products_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = { 'product': product }
     return render(request, 'products_detail.html', context)
+
+def home(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context)
