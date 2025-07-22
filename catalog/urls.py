fbe49ba9ad5706e463390fd76711f0d7d687f00b
vadimko4 <vad.informat@gmail.com>
2025-07-23 01:24:41 +0400

    add base.html, products_detail.html, my_tags.py, update catalog/urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home
+from catalog.views import contacts, home, products_detail
 
 app_name = CatalogConfig.name
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('products/<int:pk>/', products_detail, name='products_detail'),
 ]
