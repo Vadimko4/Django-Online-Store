4f4d6049776be98fac530565410bfae6a07c6220
vadimko4 <vad.informat@gmail.com>
2025-07-24 14:18:59 +0400

    add blog application, update settings, urls, models - add blog_record

--- catalog/models.py
+++ catalog/models.py
@@ -37,3 +37,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class blog_record(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Заголовок', help_text='Введите заголовок')
+    content = models.TextField(blank=False, null=False, verbose_name='Содержимое', help_text='Введите текст')
+    preview = models.ImageField(upload_to='blog_records/photo', blank=True, null=True, verbose_name='Изображение',
+                              help_text='Загрузите изображение')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания',
+                                      help_text='Укажите дату создания')
+    published = models.BooleanField(verbose_name='Опубликовано', help_text='Укажите статус', default=True)
+    views_counter = models.PositiveIntegerField(verbose_name='Количество просмотров',
+                                                help_text='Укажите количество просмотров', default=0)
+
+    class Meta:
+        verbose_name = 'Запись блога'
+        verbose_name_plural = 'Записи блога'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catalog',
+    'blog',
 ]
 
 MIDDLEWARE = [
