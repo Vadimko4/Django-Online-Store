78a4dcd15358ed79ae674f03b6381910965efa37
vadimko4 <vad.informat@gmail.com>
2025-07-24 14:47:13 +0400

    update blog/admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog_record
+
+
+@admin.register(Blog_record)  # Регистрируем модель
+class BlogAdmin(admin.ModelAdmin):
+    # Настраиваем поля, которые будем выводить в админке
+    list_display = ('id', 'title', 'content','published')
+    # По чему будем делать фильтрацию
+    list_filter = ('title',)
+    # По чему у нас будет поиск
+    search_fields = ('title', 'content')
