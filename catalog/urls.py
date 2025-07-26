f7b031a392af60ac2e55b98864746154dff64ea5
vadimko4 <vad.informat@gmail.com>
2025-07-26 16:43:24 +0400

    update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,6 +9,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('home/', ProductListView.as_view(), name='home'),
+    path('products/', ProductListView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='products_detail'),
     path('products/create/', ProductCreateView.as_view(), name='product_create'),
