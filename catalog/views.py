828c4bee201a6d3f0ba2dba07fbe30a7df61f54a
vadimko4 <vad.informat@gmail.com>
2025-07-26 13:13:05 +0400

    add updating of products, buttons "create product", "update product"

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, TemplateView, CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView
 
 from catalog.forms import ProductForm
 from catalog.models import Product
@@ -22,3 +22,11 @@ class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:home')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('catalog:products_detail', args=[self.kwargs.get('pk')])
