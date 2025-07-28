7c22e32501a31c7012a4f6bd74be43675f763a5b
vadimko4 <vad.informat@gmail.com>
2025-07-29 01:25:31 +0400

    add user_form.html, update urls.py

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
-    # path('logout/', LogoutView.as_view(next_page=''), name='logout'),
+    path('logout/', LogoutView.as_view(next_page=''), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
     # path('email-confirm/<str:token>/', email_verification, name='email-confirm')
 ]
