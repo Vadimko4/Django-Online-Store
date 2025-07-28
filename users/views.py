340b82ee02059db4d17120f067e44ae5a5242669
vadimko4 <vad.informat@gmail.com>
2025-07-28 23:32:57 +0400

    update users/views add UserCreateView, update users/urls.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import secrets
 
-# Create your views here.
+from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+
+# from config.settings import EMAIL_HOST_USER
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url =  reverse_lazy('users:login')
