6afb560f5da4283784e4180abcd0501079bf6f30
vadimko4 <vad.informat@gmail.com>
2025-08-03 14:35:23 +0400

    add groups.json

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 import datetime
 from django.db import models
 
+from users.models import User
+
 
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование', help_text='Введите наименование продукта')
@@ -16,6 +18,8 @@ class Product(models.Model):
     updated_at = models.DateField(blank=False, null=False, default=datetime.date.today,
                                   verbose_name='Дата изменения', help_text='Укажите дату изменения')
     published = models.BooleanField(default=False, verbose_name='Опубликовано', help_text='Введите статус публикации')
+    # owner = models.ForeignKey(User, verbose_name="Владелец", help_text="Укажите владельца продукта", blank=True,
+    #                           null=True, on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name = 'Продукт'
