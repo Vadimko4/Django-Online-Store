bc2ff87ba1b90a03946aca5be0059cd031443672
vadimko4 <vad.informat@gmail.com>
2025-08-03 22:38:31 +0400

    install redis, update settings, update catalog/urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import ContactsView, ProductDetailView, ProductListView, ProductCreateView, ProductUpdateView, \
     ProductDeleteView
+from django.views.decorators.cache import cache_page
 
 app_name = CatalogConfig.name
 
@@ -10,7 +11,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('products/', ProductListView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_detail'),
     path('products/create/', ProductCreateView.as_view(), name='product_create'),
     path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
